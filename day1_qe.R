b1d374abd7ba397fea0444487106f5a655aa876a
Jess050 <jessesmith050@gmail.com>
2018-07-26 13:06:27 +0200

    1st commit

---
+++ day1_qe.R
@@ -0,0 +1,24 @@
+# day 2: numerical ecology with R
+# 26/7
+# jesse
+
+# loading mite data
+# data("mite")
+# data(mite.env)
+# data("mite.xy")
+
+# repeat all analysis in ch2
+# book uses base R 
+# due: thurs 2nd Aug
+# using modern techniques eg. tidyverse and ggplot 
+
+# create new directory 
+# read in data (vegan) 
+# tidyverse equivalence of graphs 
+
+# bargraph in ggplot (line 68)
+# map of site locations (ggmap)
+# 
+
+
+
